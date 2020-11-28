ddc7a9234272b54c06016a08e619b4ccf03305d6
conjuringwxq <1794594886@qq.com>
2020-11-28 20:17:58 +0800

    文章列表

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="article-list">
+    <el-card shadow="hover">
+      {{ item }}
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticleList",
+  props: {
+    item: Number
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.article-list {
+  margin-bottom: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,10 @@
 <template>
   <el-row :gutter="20">
     <el-col :span="16">
-      <el-card shadow="always">
-        left
-      </el-card>
+      <article-list v-for="item in totalCount" :key="item" :item="item" />
     </el-col>
     <el-col :span="8">
-      <el-card shadow="always">
+      <el-card shadow="hover">
         right
       </el-card>
     </el-col>
@@ -22,5 +20,19 @@
 </template>
 
 <script>
-export default {};
+import { reactive, toRefs } from "vue";
+import ArticleList from "../components/ArticleList";
+
+const totalCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default {
+  components: { ArticleList },
+  setup() {
+    const state = reactive({
+      totalCount
+    });
+
+    return { ...toRefs(state) };
+  }
+};
 </script>
