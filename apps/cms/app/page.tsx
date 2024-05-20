21a6f0fe39c594269921187fa0988a4f00aaf40f
wcywxq <wcywxq@gmail.com>
2024-05-20 12:11:29 +0800

    chore: add new pro

---
+++ apps/cms/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Button type="primary">Button</Button>
+    </main>
+  );
+}
